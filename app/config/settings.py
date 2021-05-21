0bb1467612342b5aef4f55595cb84ae113fd0361
Anawat <anawat@playtorium.co.th>
2021-05-21 10:54:59 +0700

    edit setting to get .env

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 SETTINGS = {
     'mysql': {
-        'host': 'u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        'user': 'vj7o88y37zsg83r6',
-        'password': 'jf05cwc9o5p4uwbf',
-        'db': 'mso0rk5x4zdi3lhh',
-        'port': '3306'
+        'host': getenv('DB_HOST'),
+        'user': getenv('DB_USERNAME'),
+        'password':getenv('DB_PASSWORD'),
+        'db':getenv('DB_DATABASE'),
+        'port':getenv('DB_PORT')
     },
 }
